c3c6405e952b50cac0f0282bb850a9c04d2f5097
Karen <karen.yonea@hotmail.com>
2019-05-03 15:46:36 -0300

    Fiz o A, GLORIA A DEUX(só ele sabe se funciona)

--- proj3/exerA.f90
+++ proj3/exerA.f90
@@ -1,10 +1,9 @@
-PROGRAM exerA
+PROGRAM exerA1
   IMPLICIT NONE
-  REAL(8) :: m, p, v, i, t, dt
-
-  !Definindo os valores de massa e potência
-  m = 70.0;
-  p = 400.0;
+  REAL(8), PARAMETER :: m = 70.0 !Massa kg
+  REAL(8), PARAMETER :: p = 400.0 !Potência W
+  REAL(8) :: v, t, dt
+  INTEGER :: i, n !Contador e número de iterações
 
   !Arquivo de saida
   OPEN(UNIT=2,FILE="velA_out.dat")
@@ -14,9 +13,17 @@ PROGRAM exerA
   READ(*,*)dt
   READ(*,*)t
 
+  !Escreve primeira linha no arquivo de saída
+  WRITE(2,*)0, v
+
+  n = INT(t/dt)
+
   !Cálculo iterativo da velocidade
-  DO i = 0,
+  DO i = 1, n
+    !Calcula a nova velocidade
     v = v + p/(m*v)*dt
+    t = i*dt
+    !Escreve o valor de velocidade para cada t 
     WRITE(2,*)t, v
   END DO
 END PROGRAM exerA
