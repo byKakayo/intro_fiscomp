e6285d6996f453bf292773d471d6b0eef75d343f
Karen <karen.yonea@hotmail.com>
2019-05-20 10:50:22 -0300

    n sei oq eu to fazendo

--- proj5/exer1.f90
+++ proj5/exer1.f90
@@ -1,3 +1,27 @@
 PROGRAM exer1
-  IMPLICIT NONE 
+  IMPLICIT NONE
+  REAL(8) :: r, t, d, lambda
+  INTEGER(8) :: x0, x, e, i
+  INTEGER(8), PARAMETER :: iMax = 1000 !Valor maximo de iterações
+
+  !Arquivo de saída
+  OPEN(UNIT=2,FILE="dist_out.dat")
+
+  !Ler do terminal os valores de:
+  !Constante
+  !Valor inicial de individuos
+  !
+  READ(*,*)r
+  READ(*,*)x0
+  READ(*,*)e
+
+  DO i = 1,
+    !Escreve a população e distancia para cada t
+    WRITE(2, *)t, x, d
+  END DO
+
+  PRINT *, !lambda pela exponencial, lambda
+
+  !Fecha arquivo de saída
+  CLOSE(2)
 END PROGRAM exer1
